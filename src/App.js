536d318e329834fe85f53d3747397e89b3db1cd8
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-06-24 16:20:54 +0530

    added serach functionality in application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Contact from "./Contact";
 
@@ -9,32 +8,51 @@ class App extends Component {
       {
         name: "Abhishek Pathak",
         handle: "@abc",
-        imageUrl:
-          "C:\\Users\\AP185396\\Documents\\contact-app\\public\\logo512.png",
       },
       {
         name: "Tommy",
         handle: "@tbc",
-        imageUrl: "",
       },
       {
         name: "Abhishek Pathak",
         handle: "@ac",
-        imageUrl: "",
       },
       {
         name: "Abhishek Pathak",
         handle: "@tommy",
-        imageUrl: "",
       },
     ],
+    query: "",
   };
   render() {
     return (
       <div className="main">
-        {this.state.contacts.map((contact) => {
-          return <Contact name={contact.name} handle={contact.handle} />;
-        })}
+        <form className="search-form">
+          <h4>Search: </h4>&nbsp;&nbsp;&nbsp;
+          <input
+            type="text"
+            placeholder="Search Contacts"
+            value={this.state.query}
+            style={{
+              width: "350px",
+              height: "30px",
+              border: "2px solid",
+              borderRadius: "1%",
+            }}
+            onChange={(e) => {
+              this.setState({ query: e.target.value });
+            }}
+          />
+        </form>
+        {this.state.contacts
+          .filter((contact) => {
+            return contact.name
+              .toLowerCase()
+              .includes(this.state.query.toLowerCase());
+          })
+          .map((contact) => {
+            return <Contact name={contact.name} handle={contact.handle} />;
+          })}
       </div>
     );
   }
